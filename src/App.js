eb08729f8ef3852e0c34d2d589737c49e51cdea1
Rutuja-ttpl <saharerutuja.think@gmail.com>
2024-10-21 08:05:03 +0530

    added changes for cakes page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './component/Layout';
+import Home from './component/Home';
+import Contact from './component/Contact';
+import Order from './component/Order';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+             <Route path="order" element={<Order />} />
+            {/* <Route path="login" element={<LoginReg />} />
+            <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
+            <Route path="reset" element={<ResetPassword />} /> */}
+          </Route>
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
